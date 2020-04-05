13cafc123ff738d72726bd5effced0d4d7b8cae4
abolfazlshirazi <45438565+abolfazlshirazi@users.noreply.github.com>
2020-04-05 18:52:57 +0200

    Delete

--- Problems/G08/G08_OBJ.m
+++
@@ -1,8 +0,0 @@
-function y = G08_OBJ(x)
-    % Problem:          G08
-    % Function Type:    Objectives
-    % Date:             Nov. 2019
-    % By:               Abolfazl Shirazi (ashirazi@bcamath.org)
-    
-    y = -(sin(2*pi*x(1))^3*sin(2*pi*x(2)))/(x(1)^3*(x(1)+x(2)));
-end
--- Problems/G13/G13_OBJ.m
+++
@@ -1,8 +0,0 @@
-function y = G13_OBJ(x)
-    % Problem:          G13
-    % Function Type:    Objectives
-    % Date:             Nov. 2019
-    % By:               Abolfazl Shirazi (ashirazi@bcamath.org)
-    
-    y = exp(prod(x));
-end
