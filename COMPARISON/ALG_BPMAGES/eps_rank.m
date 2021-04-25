d158976dcadee6acd1cba3dcf006eec1b2b97b7a
abolfazlshirazi <45438565+abolfazlshirazi@users.noreply.github.com>
2021-04-25 18:59:09 +0200

    Delete

--- COMPARISON/ALG_BPMAGES/eps_rank.m
+++
@@ -1,9 +0,0 @@
-function [z]=eps_rank(f1,cv1,f2,cv2,epsilon)
-    if cv1 == cv2
-        z=(f1 <= f2);  
-    elseif (cv1 <= epsilon) && (cv2<= epsilon)
-        z=(f1 <= f2);
-    else
-        z=(cv1 < cv2);
-    end
-end
--- COMPARISON/ALG_BPMAGES/lex_rank.m
+++
@@ -1,7 +0,0 @@
-function [z]=lex_rank(f1,cv1,f2,cv2)
-    if (cv1 == 0 && cv2 == 0) 
-        z=(f1 <= f2);  
-    else
-        z=(cv1 <= cv2);
-    end
-end
