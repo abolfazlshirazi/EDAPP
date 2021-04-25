f19a5475a429838d6796e8ba80a11527e6c39e69
abolfazlshirazi <45438565+abolfazlshirazi@users.noreply.github.com>
2021-04-25 18:56:59 +0200

    Upload

--- COMPARISON/ALG_COLSHADE/get_svc.m
+++
@@ -1,6 +0,0 @@
-function [svc, h_sum] = get_svc(g, h, epsilon)
-	g = max(g, 0);
-	h = max(abs(h) - epsilon, 0);
-	svc = sum(g, 2) + sum(h, 2);
-    h_sum = min(h);					% for evaluating feasibility independently
-end
